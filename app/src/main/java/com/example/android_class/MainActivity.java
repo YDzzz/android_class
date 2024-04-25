255ba011570c4576787bca8be3ff309c0cf386c4
YDzzz <1692219062wang@gmail.com>
2024-04-25 17:17:15 +0800

    photoshop

--- app/src/main/java/com/example/android_class/MainActivity.java
+++ app/src/main/java/com/example/android_class/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.android_class;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        SeekBar seekBar = findViewById(R.id.seekbar);
+        ImageView imageView = findViewById(R.id.image);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                imageView.setImageAlpha(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
     }
 }
